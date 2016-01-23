1f2dee213d37b4582fd032fa9bb4a67bbac03479
Adam Sinck <absinck+github@grueslayer.com>
2016-01-23 14:11:56 -0700

    optimized the bindings

--- keybindings.py
+++ keybindings.py
@@ -1,8 +1,19 @@
-'''
+#this file has almost all of the key bindings for the main program.
+
+#if I ever rebind the almost 200 combinations again, I am so writing a
+#secondary program to format everything for me. Careful listing,
+#grouping, and reassignment of all the keys took days; typing in the
+#assignments and formatting everything took FOREVER. Seriously.
+
+
+#some random helpful info copied off a site:
+''' 
 userInput.bind("<Escape>", lambda(nothing): destroyThis(userDialog))
 userInput.bind("<Return>", functionB)
 
-The user pressed the Enter key. You can bind to virtually all keys on the keyboard. For an ordinary 102-key PC-style keyboard, the special keys are
+You can bind to virtually all keys on the keyboard. For an ordinary 102-key PC-style keyboard, the special keys are
+
+
 Cancel (the Break key),
 BackSpace,
 Tab,
@@ -30,7 +41,13 @@ and Scroll_Lock.
 
 Use in format:
 <Control-key>
+
+# Little-known feature of Tk, it allows to bind an event to
+# multiple keystrokes
+self.bind("<Control-Key-%s><Key>" % k,
+          lambda event, a=a: self.insert_accented(event.char, a))
 '''
+
 #modifiers
 ctrl = "<Control-"
 alt = "<Alt-"
@@ -58,144 +75,122 @@ Z = "Z>";
 bindings = {
     #special characters
     enter:"\n",
-    less:"<",
     space:" ",
 
     #actual bindings
-
-    #make ctrl-, and ctrl-. jump to beginning and end?
-    #bind C-? to a word completion predictor?
+    ctrl+q:"who",    alt+q:"qu",     ctrl+Q:"que",    alt+Q:"dle",
+    ctrl+w:"what",   alt+w:"wh",     ctrl+W:"was",    alt+W:"ware",
+    ctrl+e:"when",   alt+e:"ere",    ctrl+E:"ese",    alt+E:"ele",
+    ctrl+r:"where",  alt+r:"tri",    ctrl+R:"try",    alt+R:"tran",
+    ctrl+t:"why",    alt+t:"tion",   ctrl+T:"tian",   alt+T:"ity",
+    ctrl+y:"how",    alt+y:"st",     ctrl+Y:"str",    alt+Y:"thr",
+    ctrl+u:"who",    alt+u:"ie",     ctrl+U:"ea",     alt+U:"ent",
+    ctrl+i:"would",  alt+i:"ing",    ctrl+I:"ang",    alt+I:"ong",
+    ctrl+o:"which",  alt+o:"ou",     ctrl+O:"uou",    alt+O:"ough",
+    ctrl+p:"while",  alt+p:"ple",    ctrl+P:"pre",    alt+P:"per",
     
-    ctrl+a:"able",   alt+a:"ible",   ctrl+A:"ice",    alt+A:"act",
-    ctrl+b:"ang",    alt+b:"ary",    ctrl+B:"ion",    alt+B:"ian",
-    ctrl+c:"ie",     alt+c:"chr",    ctrl+C:"cess",   alt+C:"cont",
-    ctrl+d:"ea",     alt+d:"ect",    ctrl+D:"ent",    alt+D:"der",
-    ctrl+e:"ere",    alt+e:"ese",    ctrl+E:"ele",    alt+E:"eve",
-    ctrl+f:"of",     alt+f:"is",     ctrl+F:"mem",    alt+F:"ble",
-    ctrl+g:"gh",     alt+g:"ght",    ctrl+G:"gram",   alt+G:"gen",
-    ctrl+h:"ch",     alt+h:"sh",     ctrl+H:"chip",   alt+H:"ship",
-    ctrl+i:"ing",    alt+i:"ity",    ctrl+I:"ire",    alt+I:"ive",
-    ctrl+j:"ich",    alt+j:"ide",    ctrl+J:"igh",    alt+J:"ign",
-    ctrl+k:"ink",    alt+k:"can",    ctrl+K:"geom",   alt+K:"int",
-    ctrl+l:"less",   alt+l:"ly",     ctrl+L:"lex",    alt+L:"ld",
-    ctrl+m:"ment",   alt+m:"main",   ctrl+M:"mult",   alt+M:"nt",
-    ctrl+n:"nce",    alt+n:"inter",  ctrl+N:"in",     alt+N:"ness",
-    ctrl+o:"ou",     alt+o:"ough",   ctrl+O:"on",     alt+O:"ong",
-    ctrl+p:"pro",    alt+p:"ple",    ctrl+P:"pos",    alt+P:"per",
-    ctrl+q:"qu",     alt+q:"que",    ctrl+Q:"com",    alt+Q:"comp",
-    ctrl+r:"tri",    alt+r:"try",    ctrl+R:"ure",    alt+R:"ture",
-    ctrl+s:"sion",   alt+s:"st",     ctrl+S:"sian",   alt+S:"str",
-    ctrl+t:"tion",   alt+t:"th",     ctrl+T:"tian",   alt+T:"thr",
-    ctrl+u:"uct",    alt+u:"uou",    ctrl+U:"it",     alt+U:"dle", #C-U has issues
-    ctrl+v:"ivi",    alt+v:"ove",    ctrl+V:"tran",   alt+V:"ten",
-    ctrl+w:"wh",     alt+w:"ware",   ctrl+W:"ance",   alt+W:"ence",
-    ctrl+x:"nd",     alt+x:"ass",    ctrl+X:"age",    alt+X:"ant",
-    ctrl+y:"sys",    alt+y:"use",    ctrl+Y:"ed",     alt+Y:"sub",
-    ctrl+z:"ize",    alt+z:"con",    ctrl+Z:"gent",   alt+Z:"plan",
-
-
-    #ose?
+    ctrl+a:"this",   alt+a:"ose",    ctrl+A:"ore",    alt+A:"one",
+    ctrl+s:"there",  alt+s:"sion",   ctrl+S:"sian",   alt+S:"ism",
+    ctrl+d:"these",  alt+d:"con",    ctrl+D:"cont",   alt+D:"can",
+    ctrl+f:"the",    alt+f:"ed",     ctrl+F:"nd",     alt+F:"nt",
+    ctrl+g:"that",   alt+g:"gh",     ctrl+G:"ght",    alt+G:"ight",
+    ctrl+h:"than",   alt+h:"ch",     ctrl+H:"sh",     alt+H:"th",
+    ctrl+j:"they",   alt+j:"ass",    ctrl+J:"age",    alt+J:"ant",
+    ctrl+k:"them",   alt+k:"act",    ctrl+K:"ect",    alt+K:"uct",
+    ctrl+l:"those",  alt+l:"ly",     ctrl+L:"le",     alt+L:"ld",
     
-    ctrl+"Key-1>":"first", ctrl+"Key-2>":"second", ctrl+"Key-3>":"third",
-    ctrl+"Key-4>":"fourth", ctrl+"Key-5>":"fifth",
+    ctrl+z:"thing",  alt+z:"ice",    ctrl+Z:"ide",    alt+Z:"ize",
+    ctrl+x:"think",  alt+x:"able",   ctrl+X:"ible",   alt+X:"ble",
+    ctrl+c:"and",    alt+c:"ance",   ctrl+C:"ence",   alt+C:"nce",
+    ctrl+v:"are",    alt+v:"eve",    ctrl+V:"ive",    alt+V:"ove",
+    ctrl+b:"any",    alt+b:"ion",    ctrl+B:"ian",    alt+B:"ize",
+    ctrl+n:"add",    alt+n:"ness",   ctrl+N:"less",   alt+N:"cess",
+    ctrl+m:"ave",    alt+m:"ment",   ctrl+M:"main",   alt+M:"mult",
+
+    #pairs of lines are physical keys on the keyboard
+    #one line break is to separate keys in a group
+    #two line breaks separate groups
     
-    ctrl+"6>":"if", ctrl+"7>":"then", ctrl+"8>":"else",
-    ctrl+"9>":"not", ctrl+"0>":"for", 
+    ctrl+"Key-1>"       : "first",   alt+"Key-1>"        : "in",           
+    ctrl+"Key-exclam>"  : "cap",     alt+"exclam>"       : "sys",
 
-    ctrl+"quoteleft>":"they",
-    ctrl+"asciitilde>":"on",
-    ctrl+"Key-exclam>":"est",
-    ctrl+"at>":"at",
-    ctrl+"numbersign>":"point",
-    ctrl+"dollar>":"plane",
-    ctrl+"percent>":"line",
-    ctrl+"asciicircum>":"definition",
-    ctrl+"ampersand>":"government",
-    ctrl+"asterisk>":"who",
-    ctrl+"parenleft>":"what",
-    ctrl+"parenright>":"when",
-    ctrl+"minus>":"where",
-    ctrl+"underscore>":"why",
-    ctrl+"plus>":"how",
-    ctrl+"equal>":"would",
+    ctrl+"Key-2>"       : "second",  alt+"Key-2>"        : "on",           
+    ctrl+"at>"          : "lex",     alt+"at>"           : "les",
+
+    ctrl+"Key-3>"       : "third",   alt+"Key-3>"        : "is",           
+    ctrl+"numbersign>"  : "tune",    alt+"numbersign>"   : "plan",
+
+    ctrl+"Key-4>"       : "fourth",  alt+"Key-4>"        : "of",           
+    ctrl+"dollar>"      : "icle",    alt+"dollar>"       : "mem",
+
+    ctrl+"Key-5>"       : "fifth",   alt+"Key-5>"        : "at",           
+    ctrl+"percent>"     : "ned",     alt+"percent>"      : "der",
     
-    ctrl+"bracketleft>":"the",
-    ctrl+"bracketright>":"these",
-    ctrl+"braceleft>":"this",
-    ctrl+"braceright>":"this is",
-    ctrl+"bar>":"is a",
-    ctrl+"backslash>":"there",
-
-    ctrl+"semicolon>":"but",
-    ctrl+"colon>":"while",
-    ctrl+"quoteright>":"poly",
-    ctrl+"quotedbl>":"compute",
-
-    ctrl+"less>":"any",
-    ctrl+"greater>":"that",
-    ctrl+"slash>":"to",
-    ctrl+"question>":"from",
-
-
-
-    alt+"Key-1>":"and", alt+"Key-2>":"are", alt+"Key-3>":"add",
-    alt+"Key-4>":"ack", alt+"Key-5>":"ase",
     
-    alt+"6>":"some", alt+"7>":"few", alt+"8>":"many",
-    alt+"9>":"thing", alt+"0>":"think", 
+    ctrl+"6>":"if",     alt+"6>":"it",
 
-    alt+"quoteleft>":"ave",
-    alt+"asciitilde>":"",
-    alt+"Key-exclam>":"one",
-    alt+"at>":"",
-    alt+"numbersign>":"",
-    alt+"dollar>":"ill",
-    alt+"percent>":"which",
-    alt+"asciicircum>":"ism",
-    alt+"ampersand>":"was",
-    alt+"asterisk>":"ore",
-    alt+"parenleft>":"pre",
-    alt+"parenright>":"", #not working?
-    alt+"minus>":"les",
-    alt+"underscore>":"end",
-    alt+"plus>":"tune",
-    alt+"equal>":"ake",
+    ctrl+"7>":"else",   alt+"7>":"int",
+
+    ctrl+"8>":"then",   alt+"8>":"ary",
+
+    ctrl+"9>":"not",    alt+"9>":"est",
+
+    ctrl+"0>":"for",    alt+"0>":"ll",
+
+
+    ctrl+"quoteleft>"   : "but",     alt+"quoteleft>"    : "con",      
+    ctrl+"asciitilde>"  : "com",     alt+"asciitilde>"   : "comp",
+
+
+    ctrl+"minus>"       : "to",      alt+"minus>"        : "tune",         
+    ctrl+"underscore>"  : "ture",    alt+"underscore>"   : "ure",
+
+    ctrl+"equal>"       : "from",    alt+"equal>"        : "out",          
+    ctrl+"plus>"        : "ea",      alt+"plus>"         : "igh",
+
+
+    ctrl+"bracketleft>" : "some",    alt+"bracketleft>"  : "ign",
+    ctrl+"braceleft>"   : "igh",     alt+"braceleft>"    : "ivi",
+                                     
+    ctrl+"bracketright>": "few",     alt+"bracketright>" : "gen",
+    ctrl+"braceright>"  : "gent",    alt+"braceright>"   : "end",
+                                     
+    ctrl+"backslash>"   : "many",    alt+"backslash>"    : "inter",
+    ctrl+"bar>"         : "chr",     alt+"bar>"          : "sub",
+
     
-    alt+"bracketleft>":"before",
-    alt+"bracketright>":"after",
-    alt+"braceleft>":"those",
-    alt+"braceright>":"them",
-    alt+"bar>":"tch",
-    alt+"backslash>":"against",
+    ctrl+"semicolon>"   : "before",  alt+"semicolon>"    : "after", 
+    ctrl+"colon>"       : "against", alt+"colon>"        : "ten",
 
-    alt+"semicolon>":"est",
-    alt+"colon>":"cap",
-    alt+"quoteright>":"ose",
-    alt+"quotedbl>":"icle",
+    ctrl+"quoteright>"  : "with",    alt+"quoteright>"   : "ike",   
+    ctrl+"quotedbl>"    : "ink",     alt+"quotedbl>"     : "ake",
 
-    alt+"less>":"ick",
-    alt+"greater>":"out",
-    alt+"slash>":"ned",
-    alt+"question>":"ike",
-#    these are reserved
-#    ctrl+"comma>":"",
-#    ctrl+"period>":"",
+
+    ctrl+"comma>"       : "ich",     alt+"comma>"        : "tch",   
+    ctrl+"less>"        : "ick",     alt+"less>"         : "ack",
+
+    ctrl+"period>"      : "use",     alt+"period>"       : "ose",   
+    ctrl+"greater>"     : "chip",    alt+"greater>"      : "ship",
+
+    ctrl+"slash>"       : "pro",     alt+"slash>"        : "pos",   
+    ctrl+"question>"    : "gram",    alt+"question>"     : "graph"
+
     
-    # ctrl+"!>":"", ctrl'">':'', ctrl"#>":"", ctrl"$>":"", ctrl"%>":"",
-    # ctrl+"&>":"", ctrl"'>":"", ctrl"(>":"", ctrl")>":"", ctrl"*>":"",
-    # ctrl+"+>":"", ctrl",>":"", ctrl"->":"", ctrl".>":"", ctrl"/>":"",
-    # ctrl+":>":"", ctrl";>":"", ctrl"=>":"", ctrl">>":"", ctrl"?>":"",
-    # ctrl+"@>":"", ctrl"[>":"", ctrl"\\>":"", ctrl"]>":"", ctrl"^>":"",
-    # ctrl+"_>":"", ctrl"`>":"", ctrl"{>":"", ctrl"|>":"", ctrl"}>":"",
-    # ctrl+"~>":""
 
-#    # Little-known feature of Tk, it allows to bind an event to
-#    # multiple keystrokes
-#    self.bind("<Control-Key-%s><Key>" % k,
-#              lambda event, a=a: self.insert_accented(event.char, a))
+#unassigned:
+# ^ = ctrl+"asciicircum>":"",
+# & = ctrl+"ampersand>":"",
+# * = ctrl+"asterisk>":"",
+# ( = ctrl+"parenleft>":"",
+# ) = ctrl+"parenright>":"",
 
-    #< and > might need special binding
+    #keys that have issues:
+    #alt-` (OS level command similar to alt-tab)
+    #ctrl-~, ctrl-!, ctrl-@ all don't fire
+    #ctrl-U (OS level command, some sort of UTF8 insertion command)
 }
+
+
 letters = {
     "0":"0", "1":"1", "2":"2", "3":"3", "4":"4",
     "5":"5", "6":"6", "7":"7", "8":"8", "9":"9",
@@ -217,7 +212,7 @@ letters = {
     "!":"!", '"':'"', "#":"#", "$":"$", "%":"%",
     "&":"&", "'":"'", "(":"(", ")":")", "*":"*",
     "+":"+", ",":",", "-":"-", ".":".", "/":"/",
-    ":":":", ";":";", "=":"=", ">":">", #"<":"<", 
+    ":":":", ";":";", "=":"=", ">":">", "<less>":"<", #less doesn't like being bound
     "?":"?", "@":"@", "[":"[", "\\":"\\", "]":"]",
     "^":"^", "_":"_", "`":"`", "{":"{", "|":"|",
     "}":"}", "~":"~"
